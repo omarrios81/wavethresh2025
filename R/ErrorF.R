880be0cbba637ab9559190f69cbd96b2a0ddb56e
omarrios81 <60475030+omarrios81@users.noreply.github.com>
2025-04-25 10:57:05 -0500

    Update ErrorF.R

--- R/ErrorF.R
+++ R/ErrorF.R
@@ -1,1 +1,14 @@
+errorF <- function(y, vec) {
+  SMem <- matrix(NA, nrow = ncol(vec), ncol = 1)
+  rownames(SMem) <- names(vec)
+  colnames(SMem) <- "RMSE"
+  Serr <- NULL
+  for (j in 1:ncol(vec)) {
+    for (i in 1:length(y)) {
+      Serr[i] <- (vec[i, j] - y[i])^2
+    }
+    SMem[j, ] <- sqrt(sum(Serr)/length(y))
+  }
+  return(SMem)
+}
 
